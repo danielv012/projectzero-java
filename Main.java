f393d04423e1cc007a938256c59be2c18c133f8f
Daniel Vayman <me@rockie.live>
2022-08-30 18:45:47 -0500

    Initial commit of Main class. Next step will be implementing first program logic.

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * Main
+ */
+import java.util.*;
+
+public class Main {
+
+    public static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+        System.out.print("Please enter your name: ");
+        String daniel = "";
+        daniel = s.nextLine();
+        System.out.println(daniel);
+        
+    }
+    
+}
